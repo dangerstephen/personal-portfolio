0a41f9a77a7873168a275c4c091a38a6750e30ca
dangerstephen <dangersteve1@gmail.com>
2016-12-12 23:20:20 -0800

    added my picture to main landing page

--- js/new-age.js
+++ js/new-age.js
@@ -1,4 +1,14 @@
 (function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
@@ -18,4 +28,4 @@
         }
     })
 
-})(jQuery); 
+})(jQuery); // End of use strict
